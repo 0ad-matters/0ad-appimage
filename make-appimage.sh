9e042c7c4ddebf3f92133aca5046308e3a1317b3
andy5995 <arch_stanton5995@protonmail.com>
2023-01-17 10:53:00 -0600

    remove version file
    [skip ci]

--- make-appimage.sh
+++ make-appimage.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
+
+
 export WORKSPACE="/0ad"
+export VERSION=${VERSION:="0.0.26-alpha"}
+
+echo "Version is set to '$VERSION'"
+echo "use 'VERSION=<version> $0' to change it."
+echo "Waiting 10 seconds to start, hit CTRL-C now to cancel..."
+
+read -t 10
+
+set -ev
 
 docker run -it --rm \
-  -u 0ad \
-  -e VERSION=$(cat version)  \
+  -u 0ad \-
+  -e VERSION=$VERSION  \
   -e ARCH=x86_64 \
   -e WORKSPACE \
   -e HOSTUSER=$USER \
