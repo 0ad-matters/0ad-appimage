e1bba429b6457217f7c5e656d29438ec2bcd6539
andy5995 <arch_stanton5995@protonmail.com>
2024-02-09 03:02:49 -0600

    Use linuxdeploy action [skip ci]
    I was going to try to build an arm64 and arm/v7 image using my new
    linuxdeploy action at
    https://github.com/andy5995/linuxdeploy-build-helper/, but I didn't get
    a chance to fully test this because I can't get 0ad-0.0.26-alpha to
    build on Ubuntu focal anymore (or maybe I built it on bionic last).
    
    The SpiderMonkey build fails. The error message looks a bit different
    than normal compiler errors, but I think this is the cause:
    
    "ValueError: invalid literal for int() with base 16: 'file'"
    
    Some Debian patches might fix the build:
    https://packages.debian.org/bookworm/0ad

--- workflow.sh
+++ workflow.sh
@@ -8,30 +8,39 @@ cmp_substr () {
   return $?
 }
 
-# 0ad signing keys
-# key for a26
-MINISIGN_KEY=RWTWLbO12+ig3lUExIor3xd6DdZaYFEozn8Bu8nIzY3ImuRYQszIQyyy
-# key for a25
-# MINISIGN_KEY=RWT0hFWv57I2RFoJwLVjxEr44JOq/RkEx1oT0IA3PPPICnSF7HFKW1CT
+set -ev
 
-export -p
+WORKSPACE=${WORKSPACE:-$ACTION_WORKSPACE}
+WORKSPACE=${WORKSPACE:-$(pwd)}
+if [[ "$WORKSPACE" != /* ]]; then
+  echo "The workspace path must be absolute"
+  exit 1
+fi
+test -d "$WORKSPACE"
 
-# This var is set in the the docker container
-if [ -z "DOCKER_0AD_BUILD" ]; then
-  echo "This script is intended to be run inside a docker container."
-  echo "(hint: andy5995/0ad-build-env:focal)"
+SOURCE_ROOT=${SOURCE_ROOT:-$ACTION_SOURCE_ROOT}
+SOURCE_ROOT=${SOURCE_ROOT:-$WORKSPACE}
+if [[ "$SOURCE_ROOT" != /* ]]; then
+  echo "The source root path must be absolute"
   exit 1
 fi
 
-set -ev
+if [ ! -r "$SOURCE_ROOT/source/main.cpp" ]; then
+  echo "set the source root!"
+  exit 1
+fi
 
-test -n "$VERSION"
-test -n "$WORKSPACE"
-APPDIR="$WORKSPACE/AppDir"
+APPDIR=${APPDIR:-"/tmp/$USER-AppDir"}
+if [ -d "$APPDIR" ]; then
+  rm -rf "$APPDIR"
+else
+  mkdir -v -p "$APPDIR"
+fi
+
+env
+export -p
+
 URI=https://releases.wildfiregames.com/rc
-
-svn=1
-cmp_substr "$VERSION" "svn" || svn=0
 
 cd $WORKSPACE
 if [ ! -e "AppRun" ]; then
@@ -39,113 +48,84 @@ if [ ! -e "AppRun" ]; then
   exit 1
 fi
 
-if [ -d "$APPDIR" ]; then
-  rm -rf "$APPDIR"
-else
-  mkdir -v -p "$APPDIR"
-fi
-
-if [ $svn -ne 1 ]; then
-  ABS_PATH_SRC_ROOT="$WORKSPACE/0ad-$VERSION"
-else
-  ABS_PATH_SRC_ROOT="$WORKSPACE/0ad-svn"
-fi
-
 cd "$WORKSPACE"
 
-# The gtk plugin is placed in this directory because this is where linuxdeploy
-# is run from later to create the AppImage. In this location, the plugin will
-# be visible to linuxdeploy when the AppImage is created
-if [ ! -r linuxdeploy-plugin-gtk.sh ]; then
-  curl -LO https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-gtk/master/linuxdeploy-plugin-gtk.sh
-  chmod +x linuxdeploy-plugin-gtk.sh
-fi
+#!/bin/bash
 
-if [ $svn -ne 1 ]; then
-  # Get, check, and extract source
-  source=0ad-$VERSION-unix-build.tar.xz
-  source_sum=$source.sha1sum
+# Set DEBIAN_FRONTEND to noninteractive
+export DEBIAN_FRONTEND=noninteractive
+# Pre-configure debconf selections to avoid prompts
+echo "keyboard-configuration  keyboard-configuration/layoutcode  string  us" | sudo debconf-set-selections
+sudo apt update && sudo apt -y upgrade && \
+  sudo apt install -y  \
+    cargo   \
+    libboost-dev    \
+    g++-8 \
+    libboost-filesystem-dev \
+    libboost-system-dev   \
+    libcurl4-gnutls-dev \
+    libenet-dev \
+    libfmt-dev   \
+    libfreetype6-dev    \
+    libgloox-dev    \
+    libicu-dev \
+    libminiupnpc-dev \
+    libogg-dev  \
+    libopenal-dev   \
+    libpng-dev  \
+    libsdl2-dev \
+    libsodium-dev   \
+    libvorbis-dev \
+    libvulkan-dev   \
+    libwxgtk3.0-gtk3-dev \
+    libxml2-dev \
+    m4 \
+    python3 \
+    rustc   \
+    zlib1g-dev
 
-  for file in $source $source_sum; do
-    if [ ! -r "$file" ]; then
-      curl -LO "$URI/$file"
-    fi
-  done
+# needed for spidermonkey build
+export SHELL=/bin/bash
+# Spidermonkey build fails with 7, 8, 9, and 10 on Ubuntu focal?
+export CC=gcc-7
+export CXX=g++-7
+# Using some Debian patches might work
+# https://packages.debian.org/bookworm/0ad
+# Giving up for now... -andy5995/2024-02-09
+cd "$SOURCE_ROOT/build/workspaces"
 
-  #if [ -n "${URI##*/rc*}" ]; then
-    #if [ ! -r $URI/$source.minisig ]; then
-      #curl -LO $URI/$source.minisig
-    #fi
-    #$MINISIGN_PATH -Vm $source -P $MINISIGN_KEY
-  #fi
-  sha1sum -c $source_sum
-  tar xJf $WORKSPACE/$source
-else
-  if [ ! -r "0ad-svn" ]; then
-    svn --quiet co https://svn.wildfiregames.com/public/ps/trunk/ 0ad-svn
-    cd 0ad-svn
-  else
-    cd "$WORKSPACE/0ad-svn"
-    svn --quiet up
-  fi
-  VERSION="$VERSION-r$(svn info --show-item revision)"
-fi
-
-# name: build
-if [ ! -r "$ABS_PATH_SRC_ROOT/source/main.cpp" ]; then
-  echo "set the source root!"
-  exit 1
-fi
-cd "$ABS_PATH_SRC_ROOT/build/workspaces"
-
-/bin/bash -c 'ionice -c3 nice -n 19 \
-  ./update-workspaces.sh \
+/bin/bash -c './update-workspaces.sh \
     -j$(nproc) && \
   make config=release -C gcc -j$(nproc)'
 
 # name: prepare AppDir
 cd $WORKSPACE
-if [ $svn -ne 1 ]; then
-  # Get, check, and extract data
-  data=0ad-$VERSION-unix-data.tar.xz
-  data_sum=$data.sha1sum
-  echo "Getting data and extracting archive..."
-  for file in $data $data_sum; do
-    if [ ! -r "$file" ]; then
-      curl -LO "$URI/$file"
-    fi
-  done
-
   #if [ -n "${URI##*/rc*}" ] && [ ! -r $URI/$data.minisig ]; then
       #curl -LO $URI/$data.minisig
   #fi
 
   #$MINISIGN_PATH -Vm $data -P $MINISIGN_KEY
-  sha1sum -c $data_sum
-  tar xJf $data
-fi
 
-cd "$ABS_PATH_SRC_ROOT"
+cd "$SOURCE_ROOT"
 install -s binaries/system/pyrogenesis -Dt $APPDIR/usr/bin
 install -s binaries/system/ActorEditor -Dt $APPDIR/usr/bin
 cd $APPDIR/usr/bin
 ln -s pyrogenesis 0ad
 for lib in libmozjs78-ps-release.so \
-        libnvcore.so    \
-        libnvimage.so   \
-        libnvmath.so    \
-        libnvtt.so
+  libnvcore.so    \
+  libnvimage.so   \
+  libnvmath.so    \
+  libnvtt.so
 do
-  patchelf --set-rpath $lib:$ABS_PATH_SRC_ROOT/binaries/system pyrogenesis
+  patchelf --set-rpath $lib:$SOURCE_ROOT/binaries/system pyrogenesis
 done
 patchelf --set-rpath libthai.so.0:$APPDIR/usr/lib ActorEditor
-patchelf --set-rpath libAtlasUI.so:$ABS_PATH_SRC_ROOT/binaries/system ActorEditor
+patchelf --set-rpath libAtlasUI.so:$SOURCE_ROOT/binaries/system ActorEditor
 # Note that binaries/system{libmoz*.so, libnv*.so, libAtlasUI.so} will be copied into
 # the $APPDIR folder automatically when linuxdeploy is run below.
-cd $ABS_PATH_SRC_ROOT
+cd $SOURCE_ROOT
 install binaries/system/libCollada.so -Dt $APPDIR/usr/lib
 install build/resources/0ad.appdata.xml -Dt $APPDIR/usr/share/metainfo
-install build/resources/0ad.desktop -Dt $APPDIR/usr/share/applications
 install build/resources/0ad.png -Dt $APPDIR/usr/share/pixmaps
 mkdir -p "$APPDIR/usr/data/config"
 cp -a binaries/data/config/default.cfg $APPDIR/usr/data/config
@@ -155,59 +135,51 @@ mkdir -p $APPDIR/usr/data/mods
 cp -a binaries/data/mods/mod $APPDIR/usr/data/mods
 
 # Hopefully prevent out-of-space failure when running on a GitHub hosted runner
-if [ -n "${GITHUB_ACTIONS}" ]; then
-  cd "${ABS_PATH_SRC_ROOT}/build/workspaces"
+if [ -n "$ACTION_WORKSPACE" ]; then
+  cd "$SOURCE_ROOT/build/workspaces"
   ./clean-workspaces.sh
 fi
 
-cd $ABS_PATH_SRC_ROOT
-if [ $svn -eq 1 ]; then
-  mkdir -p $APPDIR/usr/data/mods/public
-  binaries/system/pyrogenesis -writableRoot  \
-    -mod=mod   \
-    -archivebuild=binaries/data/mods/public  \
-    -archivebuild-output=$APPDIR/usr/data/mods/public/public.zip    \
-    -archivebuild-compress \
-    && test -f "$APPDIR/usr/data/mods/public/public.zip"
-  cp -a binaries/data/mods/public/mod.json $APPDIR/usr/data/mods/public
-else
-  cp -a binaries/data/mods/public $APPDIR/usr/data/mods
-fi
+cd $SOURCE_ROOT
+cp -a binaries/data/mods/public $APPDIR/usr/data/mods
 
-# spirv. See https://wildfiregames.com/forum/topic/104382-vulkan-new-graphics-api/
-mkdir $APPDIR/usr/data/mods/0ad-spirv
-cd $APPDIR/usr/data/mods/0ad-spirv
-curl -LO https://releases.wildfiregames.com/rc/0ad-spirv.zip
-curl -LO https://releases.wildfiregames.com/rc/0ad-spirv.zip.sha1sum
-sha1sum -c 0ad-spirv.zip.sha1sum
-rm 0ad-spirv.zip.sha1sum
-unzip 0ad-spirv.zip mod.json
+## spirv. See https://wildfiregames.com/forum/topic/104382-vulkan-new-graphics-api/
+#mkdir $APPDIR/usr/data/mods/0ad-spirv
+#cd $APPDIR/usr/data/mods/0ad-spirv
+#curl -LO https://releases.wildfiregames.com/rc/0ad-spirv.zip
+#curl -LO https://releases.wildfiregames.com/rc/0ad-spirv.zip.sha1sum
+#sha1sum -c 0ad-spirv.zip.sha1sum
+#rm 0ad-spirv.zip.sha1sum
+#unzip 0ad-spirv.zip mod.json
 
 cd "$WORKSPACE"
 
 # Hopefully prevent out-of-space failure when running on a GitHub hosted runner
 echo "Removing data from source tree (already copied to ${APPDIR})..."
-if [ -n "${GITHUB_ACTIONS}" ]; then
-  rm -rf "${ABS_PATH_SRC_ROOT}/binaries/data"
+if [ -n "$ACTION_WORKSPACE" ]; then
+  rm -rf "$SOURCE_ROOT/binaries/data"
 fi
 
 # Create the image
-DEPLOY_GTK_VERSION=3 # Variable used by gtk plugin
-ionice -c3 $TOOLS_DIR/squashfs-root/AppRun -d $APPDIR/usr/share/applications/0ad.desktop \
-  --icon-file=$APPDIR/usr/share/pixmaps/0ad.png \
-  --icon-filename=0ad \
-  --executable $APPDIR/usr/bin/pyrogenesis \
-  --library=/usr/lib/x86_64-linux-gnu/libthai.so.0 \
-  --custom-apprun=$WORKSPACE/AppRun \
-  --appdir $APPDIR \
-  --output appimage \
-  --plugin gtk
+if [ -z "ACTION_WORKSPACE" ]; then
+  export DEPLOY_GTK_VERSION=3 # Variable used by gtk plugin
+  linuxdeploy \
+    -d $SOURCE_DIR/build/resources/0ad.desktop \
+    --icon-file=$SOURCE_DIR/build/resources/0ad.png \
+    --icon-filename=0ad \
+    --executable $APPDIR/usr/bin/pyrogenesis \
+    --library=/usr/lib/x86_64-linux-gnu/libthai.so.0 \
+    --custom-apprun=$ACTION_WORKSPACE/AppRun \
+    --appdir $APPDIR \
+    --output appimage \
+    --plugin gtk
+fi
 
-DATE_STR=$(date +%y%m%d%H%M)
-OUT_APPIMAGE="0ad-$VERSION-$DATE_STR-$UBUNTU_CODENAME-$ARCH.AppImage"
-mv 0_A.D.-$VERSION-$ARCH.AppImage $OUT_APPIMAGE
-echo "Generating sha1sum..."
-sha1sum $OUT_APPIMAGE > "$OUT_APPIMAGE.sha1sum"
-cat "$OUT_APPIMAGE.sha1sum"
+#DATE_STR=$(date +%y%m%d%H%M)
+#OUT_APPIMAGE="0ad-$VERSION-$DATE_STR-$UBUNTU_CODENAME-$ARCH.AppImage"
+#mv 0_A.D.-$VERSION-$ARCH.AppImage $OUT_APPIMAGE
+#echo "Generating sha1sum..."
+#sha1sum $OUT_APPIMAGE > "$OUT_APPIMAGE.sha1sum"
+#cat "$OUT_APPIMAGE.sha1sum"
 
 exit 0
