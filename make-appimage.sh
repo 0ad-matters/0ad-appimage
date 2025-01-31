da7bb5c05a2cf5c6093517330e7ce5d87b60561b
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-01-31 22:23:30 +0100

    Fix typo

--- make-appimage.sh
+++ make-appimage.sh
@@ -227,7 +227,7 @@ REPO="0ad-appimage"
 TAG="latest"
 UPINFO="gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|$REPO|$TAG|*$ARCH.AppImage.zsync"
 
-ARCH=x86_64 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 \
+./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 \
 	-u "$UPINFO" \
 	"$APPDIR" "$OUT_APPIMAGE"
 
